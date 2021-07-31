b64f83aab1903e52360aeb6645fdcb3b6253fe22
Pliskeviski <gustavopliskeviski@gmail.com>
2021-07-31 11:57:06 -0300

    feat(gatsby): structure

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Pliskeviski",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-121602336-3",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const IndexPage = () => {
+  return <p></p>;
+};
+
+export default IndexPage;
