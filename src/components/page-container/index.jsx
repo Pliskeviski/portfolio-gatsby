a33db059c4c7bf815dcae7179c08a6170dc765d2
Pliskeviski <gustavopliskeviski@gmail.com>
2021-08-02 19:39:21 -0300

    feat(page-container): page title

--- src/components/page-container/index.jsx
+++ src/components/page-container/index.jsx
@@ -4,10 +4,16 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "@utils/global-style";
 import { theme } from "@utils/theme";
 import { StyledContainer } from "./styes";
+import { Helmet } from "react-helmet";
 
 export const PageContainer = (props) => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Gustavo Pliskeviski - Portfolio</title>
+      <link rel="canonical" href="https://pliskeviski.github.io/" />
+    </Helmet>
     <StyledContainer {...props} />
   </ThemeProvider>
 );
