93835b5d64e4588b0a3c56d1a2f8f68da504d360
Pliskeviski <gustavopliskeviski@gmail.com>
2021-08-02 19:45:32 -0300

    style(page-container): scroll margin

--- src/components/page-container/styes.js
+++ src/components/page-container/styes.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  margin: 0 10px;
+  margin: 0 4px 0 10px;
   overflow: auto;
   -ms-scroll-snap-type: y mandatory;
   scroll-snap-type: y mandatory;
